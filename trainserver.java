91bf4865e803667ae1cc1b965e28ccd1248193d3
davidrcyril <david@fyrsta.org>
2016-01-23 14:50:47 +0530

    message

---
+++ trainserver.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.*;
+public class trainserver{
+public static void main(string[] args)
+{
+try{
+ServerSocket d=new ServerSocket(1234);
+Socket server=d.accept();
+DataInputStream sin= new DataInputStream(server.getInputStream());
+String str=(String)sin.readUTF();
+System.out.println(str);
+DataOutputStream sout=new DataOutputStream(server.getOutputStream());
+tout.writeUTF("Have a great day");
+tout.close();
+server.close();
+}catch(Exception e)
+}
+}
+
