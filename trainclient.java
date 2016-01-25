9cdb612eb612e75d13d9996e03643f5d8525dee5
Siddesh Pillai <siddeshvijji@gmail.com>
2016-01-25 10:07:59 +0530

    added spaces

--- trainclient.java
+++ trainclient.java
@@ -1,17 +1,32 @@
 import java.io.*;
 import java.net.*;
+
 public class trainclient{
-public static void main(string[] args)
-{
-try{
-Socket server = new Socket(“training”,1234);
-DataInputStream cin= new DataInputStream(server.getInputStream());
-String str=(String)cin.readUTF();
-System.out.println(str);
-DataOutputStream cout=new DataOutputStream(server.getOutputStream());
-cout.writeUTF("Good Morning");
-cout.close();
-server.close();
-}catch(Exception e){System.out.println(e);} 
-}
+
+    public static void main(string[] args) {
+
+        try{
+
+            Socket server = new Socket(“training”,1234);
+
+            DataInputStream cin= new DataInputStream(server.getInputStream());
+
+            String str=(String)cin.readUTF();
+
+            System.out.println(str);
+
+            DataOutputStream cout=new DataOutputStream(server.getOutputStream());
+
+            cout.writeUTF("Good Morning");
+
+            cout.close();
+
+            server.close();
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+    }
+
 }
